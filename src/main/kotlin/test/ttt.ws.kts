803bf8d68fb868f9f264210eed39e7bc52c36e4b
cc <1092241370@qq.com>
2021-11-16 10:22:28 +0000

    part 1

---
+++ src/main/kotlin/test/ttt.ws.kts
@@ -0,0 +1,7 @@
+package test
+
+val names = listOf<String>("Mary", "John", "Hello")
+for (name in names) {
+  println("hello, $name")
+}
+
---
+++ src/main/kotlin/webserver/WebServer.kt
@@ -0,0 +1,36 @@
+package webserver
+
+// write your web framework code here:
+
+fun scheme(url: String): String {
+  return url.substringBefore(':')
+}
+
+fun host(url: String): String {
+  return url.substringBeforeLast('/').substringAfterLast('/')
+}
+
+fun path(url: String): String {
+  return ('/' + (url.split("//"))[1].substringAfter('/').substringBefore('?'))
+}
+
+fun queryParams(url: String): List<Pair<String, String>> {
+  var list = listOf<Pair<String, String>>()
+  val keys_values = url.split('?')
+  if (keys_values.size > 1) {
+    val keyvalue = keys_values[1].split('&')
+    for (k_v in keyvalue) {
+      val key = k_v.split('=')[0].toString()
+      val value = k_v.split('=')[1].toString()
+      list = list.plus(Pair(key, value))
+    }
+  }
+  else {
+    return listOf()
+  }
+  return list
+}
+
+// http handlers for a particular website...
+
+fun homePageHandler(request: Request): Response = Response(Status.OK, "This is Imperial.")
